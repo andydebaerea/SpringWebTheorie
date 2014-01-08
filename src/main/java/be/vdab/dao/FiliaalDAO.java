a8aecf2d9f1c803961a4cc6629edb67e99fc359f
Andy Debaere <andydebaerea@gmail.com>
2014-01-08 09:57:31 +0100

    test update commit

--- src/main/java/be/vdab/dao/FiliaalDAO.java
+++ src/main/java/be/vdab/dao/FiliaalDAO.java
@@ -4,10 +4,16 @@ import be.vdab.entities.Filiaal;
 
 public interface FiliaalDAO {
 	void create(Filiaal filiaal);
+
 	Filiaal read(long id);
+
 	void update(Filiaal filiaal);
+
 	void delete(long id);
+
 	Iterable<Filiaal> findAll();
+
 	Iterable<Filiaal> findByPostcodeBetween(int van, int tot);
+
 	long findAantalFilialen();
 }
